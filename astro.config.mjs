fc4c25f264083b6c5ce3b83632ff8f3c70dfb967
Dewa M. Widyakumara <widyakumara@users.noreply.github.com>
2026-08-30 15:13:12 +0700

    chore: init (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 9999,
+    allowedHosts: ["demo.widyakumara.com"],
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  outDir: "./.dist",
+  build: {
+    inlineStylesheets: "never",
+    format: "file",
+    assets: "inc",
+  },
+  vite: {
+    plugins: [],
+    esbuild: { legalComments: "none" },
+    build: {
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          hashCharacters: "base36",
+          assetFileNames: "inc/[hash:8].[ext]",
+          chunkFileNames: "inc/c[hash:7].js",
+          entryFileNames: "inc/e[hash:7].js",
+        },
+      },
+    },
+  },
+});
