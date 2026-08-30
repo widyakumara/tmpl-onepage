7ef523a5c022b82be6e9b1ef49e34c948b56e1b5
Dewa Widyakumara <widyakumara@gmail.com>
2026-08-30 17:33:37 +0700

    feat: sections

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Page from "./page.astro";
 import PageFoot from "./page.foot.astro";
 import PageHead from "./page.head.astro";
+import Section from "./section.astro";
 
-export { Page, PageFoot, PageHead };
+export { Page, PageFoot, PageHead, Section };
---
+++ src/sections/index.ts
@@ -0,0 +1,6 @@
+import About from "./about.astro";
+import Contact from "./contact.astro";
+import Hero from "./hero.astro";
+import Porfolio from "./portfolio.astro";
+
+export { About, Contact, Hero, Porfolio };
